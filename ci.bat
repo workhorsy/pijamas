5d17661e122392ef7c2eb36bd6439dbfca041bd6
Luis Panadero Guardeño <luis.panadero@gmail.com>
2021-05-07 23:36:18 +0200

    Moving silly tests to a not public subpackage

--- ci.bat
+++ ci.bat
@@ -9,5 +9,5 @@ dub build -b release --compiler=%DC%
 dub clean --all-packages -q
 
 echo "Running tests"
-dub test :silly-tests --compiler=%DC% -v
+dub test --root=tests/silly --compiler=%DC% -v
 dub test --root=tests/unit-threaded --compiler=%DC% -v
