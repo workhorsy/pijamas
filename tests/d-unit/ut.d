f31a949cf35256a0dc21600ae4bb65ce53fb6695
Luis Panadero Guardeño <luis.panadero@gmail.com>
2021-05-09 10:54:24 +0200

    Testing with d-unit

---
+++ tests/d-unit/ut.d
@@ -0,0 +1,56 @@
+module dunittest;
+
+import std.exception;
+
+import dunit;
+import pijamas : should, PiException = AssertException;
+
+class Test
+{
+    mixin UnitTest;
+
+    @Test
+    public void shouldAssertionTrue() @trusted
+    {
+      // it("returns and asserts for true",
+      {
+        true.should.be.True;
+        (1 == 1).should.be.True;
+      }
+
+      // it("throws for false",
+      {
+        (1 != 1).should.not.be.True;
+        assertThrown!PiException(false.should.be.True);
+      }
+    }
+}
+
+version(Debug_Failing_Tests) {
+  class FailingTest
+  {
+      mixin UnitTest;
+
+      @Test
+      public void failingTest()
+      {
+        10.assertEquals(0);
+        //10.should.be.equal(0);
+      }
+
+      @Test
+      public void failingTestNativeAssert()
+      {
+        assert(10 == 0);
+      }
+
+      @Test
+      public void errorTest()
+      {
+        throw new Exception("Some exception");
+      }
+  }
+}
+
+// either use the 'Main' mixin or call 'dunit_main(args)'
+mixin Main;
