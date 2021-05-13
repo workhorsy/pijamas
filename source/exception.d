56d33a51794836a2b098496e4905cb766e9f8cbb
Luis Panadero Guardeño <luis.panadero@gmail.com>
2021-05-13 18:18:55 +0200

    AssertException

--- source/exception.d
+++ source/exception.d
@@ -12,9 +12,14 @@ static if (__traits(compiles, { import unit_threaded.should : UnitTestException;
 
   public alias AssertException = UnitTestException;
 } else {
-  import core.exception : AssertError;
 
-  public alias AssertException = AssertError;
+  class AssertException : Exception
+  {
+    this(string msg, string file = __FILE__, size_t line = __LINE__) @safe pure
+    {
+      super(msg, file, line);
+    }
+  }
 }
 
 
